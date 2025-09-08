76e1af90f3fc87e42122c462d377c4eab57f453d
Manoj-587 <mkumar07959@gmail.com>
2025-09-08 10:12:09 +0530

    ..

--- springapp/src/main/java/com/examly/springapp/model/Appointment.java
+++ springapp/src/main/java/com/examly/springapp/model/Appointment.java
@@ -13,7 +13,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
